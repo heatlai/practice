19a9e9d634de7439db371d5029ccecf795411c4c
Heat Lai <hitgunlai@gmail.com>
2018-04-09 10:15:22 +0800

    change: 命名風格轉換

--- php/命名風格轉換.php
+++ php/命名風格轉換.php
@@ -11,3 +11,8 @@ function toCamelCase( $string, $capitalizeFirstCharacter = false )
 
     return $str;
 }
+
+function toSnakeCase($string)
+{
+    return strtolower(preg_replace(['/([a-z\d])([A-Z])/', '/([^_])([A-Z][a-z])/'], '$1_$2', $string));
+}
