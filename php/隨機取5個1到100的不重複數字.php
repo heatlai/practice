be2116873494e1b8d799812168c7ff3f05ef8f35
Heat Lai <hitgunlai@gmail.com>
2018-04-24 16:23:05 +0800

    update: 隨機取5個1到100的不重複數字

--- php/隨機取5個1到100的不重複數字.php
+++ php/隨機取5個1到100的不重複數字.php
@@ -1,25 +1,32 @@
 <?php
 
 //開陣列
-$rand = array();
-$rand_result = array();
+//$rand = array();
+//$rand_result = array();
 
 //數字範圍 1 ~ 100
-for($i = 1; $i <=100 ; $i++)
-{ 
-    $rand[] = $i;
-}
+$rand = range(1, 100);
+
+//for($i = 1; $i <=100 ; $i++)
+//{
+//    $rand[] = $i;
+//}
 
 //陣列隨機排序
-shuffle($rand);
+//shuffle($rand);
 
 //要取幾個數字
-for($i = 0; $i < 5 ; $i++)
-{ 
-    $rand_result[$i] = $rand[$i];
-}
+//$rand_result = array_slice($rand, 0, 5);
+
+//for($i = 0; $i < 5 ; $i++)
+//{
+//    $rand_result[$i] = $rand[$i];
+//}
+
 //砍掉$rand
-unset($rand);
+//unset($rand);
+
+$rand_result = array_rand($rand, 5);
 
 //輸出結果
 print_r($rand_result);
