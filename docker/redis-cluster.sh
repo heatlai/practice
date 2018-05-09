72704439e74ad69eabfe3fe06dac54348302b42b
Heat Lai <hitgunlai@gmail.com>
2018-05-09 11:59:27 +0800

    update: redis-cluster.sh

--- docker/redis-cluster.sh
+++ docker/redis-cluster.sh
@@ -4,10 +4,10 @@
 BASEDIR=$(dirname "$0")
 cd "$BASEDIR"
 clear
-echo "PWD:"$PWD
+echo "PWD: "$PWD
 
 USE_DOCKER_COMPOSE_YML=redis-cluster-docker-compose.yml
-echo "USE_DOCKER_COMPOSE_YML:"$USE_DOCKER_COMPOSE_YML
+echo "USE_DOCKER_COMPOSE_YML: "$USE_DOCKER_COMPOSE_YML
 
 while :
 do
@@ -15,22 +15,23 @@ do
     echo "----------------------------------------"
     echo "選擇要啟動的系統:"
     echo "----------------------------------------"
-    echo "a. Redis"
+    echo "a. Redis-Cluster"
     echo "----------------------------------------"
-    echo "1. redis"
+    echo "1. redis_1"
+    echo "2. redis_2"
     echo "----------------------------------------"
     echo "工具:"
     echo "----------------------------------------"
     echo "r. 重啟所有容器"
-    echo "c. 關閉所有容器"
-    echo "ca. 刪除所有容器"
+    echo "c. 關閉所有 docker-compose 容器"
+    echo "ca. 刪除所有 docker 容器"
     echo "l. 顯示 Docker 所有容器"
-    echo "d. 指定關閉編號容器"
-    echo "  - Example: 關閉 redis-1"
-    echo "  - input: d 1"
-    echo "rd. 指定重啟編號容器"
-    echo "  - Example: 重啟 redis-1 容器"
-    echo "  - input: rd 1"
+    echo "d. 關閉指定容器"
+    echo "  - Example: 關閉 redis_1"
+    echo "  - input: d redis_1"
+    echo "dr. 重啟指定容器"
+    echo "  - Example: 重啟 redis_1 容器"
+    echo "  - input: dr redis_1"
     echo "----------------------------------------"
     echo "q. Exit"
     echo "----------------------------------------"
@@ -60,13 +61,30 @@ do
             # 查看目前的 container
             docker ps -a
             ;;
+        d)
+            # 關閉指定容器
+            if [  "$input2" == "" ]; then
+                echo "which service do you want to shutdown?"
+            else
+                docker rm -f $input2
+            fi
+            ;;
+        dr)
+            # 重啟指定容器
+            if [  "$input2" == "" ]; then
+                echo "which service do you want to rebuild?"
+            else
+                docker rm -f $input2
+                docker-compose -f $USE_DOCKER_COMPOSE_YML up -d --build $input2
+            fi
+            ;;
         c)
             # 關閉透過 docker-compose 產生的 container
             docker-compose -f $USE_DOCKER_COMPOSE_YML down
             ;;
         ca)
             # 刪除所有 docker container
-            docker rm $(docker ps -a -q)
+            docker rm -f $(docker ps -a -q)
             ;;
         *)
             # 離開程序
