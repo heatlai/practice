11d3ddf80c1b876eb15e79c7c5c9cdfa74d73c7c
Heat Lai <hitgunlai@gmail.com>
2018-06-13 17:50:14 +0800

    update : TimeTool

--- php/TimeTool.php
+++ php/TimeTool.php
@@ -3,7 +3,7 @@
 class TimeTool
 {
     public static function convertTimeZone(
-        string $dateTime,
+        string $dateTime = "now",
         string $toTimeZone = 'Asia/Taipei',
         string $fromTimeZone = 'UTC'
     )
@@ -13,5 +13,13 @@ class TimeTool
         $date = new DateTime( $dateTime, $fromTZ );
         $date->setTimezone( $toTZ );
         return $date->format( 'Y-m-d H:i:s' );
+    }
+
+    public static function getDateInterval(string $to, string $from = 'now')
+    {
+        $toDate = new DateTime($to);
+        $fromDate = new DateTime($from);
+        $interval = $fromDate->diff($toDate);
+        return $interval->format('%R%a');
     }
 }
