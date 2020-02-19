ce3a4d0f3178eaf3ffea0a02b151a345a511a117
HeatLai <heatlai@core-tech.tw>
2020-02-19 17:32:46 +0800

    shell urlencode & urldecode

---
+++ shell/urlencode.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+urlencode() {
+  python -c 'import urllib, sys; print urllib.quote(sys.argv[1], sys.argv[2])' \
+    "$1" "$urlencode_safe"
+}
+
+urldecode() {
+  python -c 'import urllib, sys; print urllib.unquote(sys.argv[1])' \
+    "$1"
+}
+
+originStr="https://google.com/?aa=123&bb=456"
+str=$(urlencode ${originStr})
+str2=$(urlencode_safe=':/='  urlencode ${originStr})
+
+echo ${str}
+echo ${str2}
+
+echo $(urldecode ${str})
