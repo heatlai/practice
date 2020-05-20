6f02de0f60554632f10da9872fbe4433125835aa
HeatLai <heatlai@core-tech.tw>
2020-05-20 18:13:50 +0800

    leetcode

--- leetcode/287. Find the Duplicate Number.php
+++ leetcode/287. Find the Duplicate Number.php
@@ -12,3 +12,16 @@ class Solution {
         return reset($nums);
     }
 }
+
+class Solution1 {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function findDuplicate($nums)
+    {
+        $nums = array_reverse(array_diff_assoc($nums, array_unique($nums)));
+        return array_pop($nums);
+    }
+}
