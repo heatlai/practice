959d4274d2b676d7e622a143b296d15f548762f7
HeatLai <heatlai@core-tech.tw>
2020-06-11 18:12:21 +0800

    php 指數退避 + 抖動

--- php/Exponential_Backoff_And_Jitter.php
+++ php/Exponential_Backoff_And_Jitter.php
@@ -4,18 +4,30 @@
  * 指數退避 + 抖動
  * Exponential Backoff And Jitter
  */
-$cap = 1000 * 15; // 15 sec 最大等待時間
-$base = 100; // ms 基本等待時間
-$max_retries = 10; // 最大重試次數
-$attempt = 0; // 目前嘗試次數
+
+// 最大等待時間 微秒(μs)
+$cap = 1000 * 1000 * 5; // 5 sec
+// 基本等待時間 微秒(μs)
+$base = 1000 * 10; // 10 ms
+// 最大重試次數
+$max_retries = 10;
+// 目前嘗試次數
+$attempt = 0;
+// 累計等待時間
+$total_wait = 0;
 
 do {
-    echo PHP_EOL;
-    echo "第 ${attempt} 次 getStatus".PHP_EOL;
+    echo "第 ${attempt} 次重試".PHP_EOL;
 
-    $status = getStatus();
+    // Full Jitter 全抖動
+    $wait_time = min($cap, $attempt === 0 ? 0 : $base * 2 ** $attempt);
+    $sleep_micro_seconds = random_int(0, $wait_time);
+    $total_wait += $sleep_micro_seconds;
+    echo "=> 最大等待時間: ".round($wait_time / 1000)." ms, 睡 ".round($sleep_micro_seconds / 1000)." ms".PHP_EOL;
+    usleep($sleep_micro_seconds);
 
-    echo ($status ?: 'NULL')." => ";
+    // do something
+    $status = getStatus(1);
 
     if ($status === 'SUCCESS') {
         $retry = false;
@@ -27,19 +39,12 @@ do {
         error_log('Some other error occurred, so stop calling the API.');
         $retry = false;
     }
+} while ($retry && ($attempt++ < $max_retries));
 
-    if ($retry) {
-        // 抖動值
-        $jitter = min($cap, $base * 2 ** $attempt++);
-        // Full Jitter 全抖動
-        $sleep_ms = random_int(0, $jitter);
-        echo "抖動值: ${jitter} , 睡 ${sleep_ms} ms";
-        usleep($sleep_ms);
-    }
-} while ($retry && ($attempt <= $max_retries));
+echo "總共等待時間: ".round($total_wait / 1000)." ms";
 
 function getStatus($num = null)
 {
-    $array = ['SUCCESS', 'NOT_READY', 'THROTTLED'];
+    $array = ['SUCCESS', 'NOT_READY', 'THROTTLED', null];
     return $array[$num ?? random_int(0, count($array) - 1)];
 }
