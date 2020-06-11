7d2bd4e70c538f9a05277042d5ab8efa7819f84e
HeatLai <heatlai@core-tech.tw>
2020-06-11 14:37:12 +0800

    php random_password

---
+++ php/random_password.php
@@ -0,0 +1,25 @@
+<?php
+
+function random_str(
+    int $length,
+    string $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ,./<>?;:"|[]{}-=_+`~!@#$%^&*'
+) {
+    $str = '';
+    $max = mb_strlen($keyspace, '8bit') - 1;
+    if ($max < 1) {
+        throw new Exception('$keyspace must be at least two characters long');
+    }
+    for ($i = 0; $i < $length; ++$i) {
+        $str .= $keyspace[random_int(0, $max)];
+    }
+    return $str;
+}
+
+function aws_password() {
+    // AWS password policy
+    $length = 16;
+    $keyspace = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+-=[]{}|'";
+    return random_str($length, $keyspace);
+}
+
+echo aws_password();
